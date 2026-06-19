30d00ae59f71a17276c7f9b4f2db69bfcc860369
Aryan-Agrawal <142228203+aryan-ml@users.noreply.github.com>
2026-06-20 02:59:29 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # cn = 0
+        # for i in s:
+        #     if i in t:
+        #         cn += 1
+    
+        # if cn == len(t):
+        #     return True
+        # else:
+        #     return False
+
+        return True if sorted(s) == sorted(t) else False
